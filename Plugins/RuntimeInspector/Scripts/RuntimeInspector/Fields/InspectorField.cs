bd5f3a30bba64c37ecc50708f0b925aef70556bb
Sandro Forster <sandro.forster@i-xt.com>
2021-12-13 11:31:15 +0100

    Reset InspectorField.IsInteractable when transform is unbound

--- Plugins/RuntimeInspector/Scripts/RuntimeInspector/Fields/InspectorField.cs
+++ Plugins/RuntimeInspector/Scripts/RuntimeInspector/Fields/InspectorField.cs
@@ -442,6 +442,7 @@ namespace RuntimeInspectorNamespace
 
 			IsExpanded = false;
 			HeaderVisibility = RuntimeInspector.HeaderVisibility.Collapsible;
+			IsInteractable = true;
 
 			ClearElements();
 		}
